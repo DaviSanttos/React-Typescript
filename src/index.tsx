ef6564e126a95f637791b20d60ce5d03f962ba85
DaviSanttos <davicelso.santos@hotmail.com>
2024-01-17 10:42:40 -0300

    react-router-dom

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import Header from 'components/Header';
+import About from 'pages/About';
+import Contact from 'pages/Contact';
+import Home from 'pages/Home';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
